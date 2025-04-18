0248cf6544f73630363129ae2c710d263bf3a237
farah <omarmohsen769@gmail.com>
2025-04-18 15:48:34 +0200

    Ignore storage/framework directory

--- storage/framework/views/ae2abfad7e3493e89d65d3e782904bdc7345cb4f.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Forbidden')); ?>
-<?php $__env->startSection('code', '403'); ?>
-<?php $__env->startSection('message', __($exception->getMessage() ?: 'Forbidden')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Radio\Downloads\codecanyon-5Pgl9o2s-rocket-lms-learning-management-academy-script\Source\Source\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/403.blade.php ENDPATH**/ ?>
