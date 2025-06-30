1e2ad5ffce35c11b7edcac76acc03b526c0d8ae3
Abdelrhman <abdelrhman@ebdaadt.com>
2025-06-30 19:31:02 +0300

    Hide Similar Businesses, Purchase MATC SAUDI sections and footer copyright line from footer

--- resources/views/web/default/includes/footer.blade.php
+++ resources/views/web/default/includes/footer.blade.php
@@ -37,14 +37,14 @@
     </div>
 
     @php
-        $columns = ['first_column','second_column','third_column','forth_column'];
+        $columns = ['first_column','second_column'];
     @endphp
 
     <div class="container">
         <div class="row">
 
             @foreach($columns as $column)
-                <div class="col-6 col-md-3">
+                <div class="col-6 col-md-6">
                     @if(!empty($footerColumns[$column]))
                         @if(!empty($footerColumns[$column]['title']))
                             <span class="header d-block text-white font-weight-bold">{{ $footerColumns[$column]['title'] }}</span>
@@ -81,31 +81,5 @@
             </div>
         </div>
     </div>
-
-    @if(getOthersPersonalizationSettings('platform_phone_and_email_position') == 'footer')
-        <div class="footer-copyright-card">
-            <div class="container d-flex align-items-center justify-content-between py-15">
-                <div class="font-14 text-white">{{ trans('update.platform_copyright_hint') }}</div>
-
-                <div class="d-flex align-items-center justify-content-center">
-                    @if(!empty($generalSettings['site_phone']))
-                        <div class="d-flex align-items-center text-white font-14">
-                            <i data-feather="phone" width="20" height="20" class="mr-10"></i>
-                            {{ $generalSettings['site_phone'] }}
-                        </div>
-                    @endif
-
-                    @if(!empty($generalSettings['site_email']))
-                        <div class="border-left mx-5 mx-lg-15 h-100"></div>
-
-                        <div class="d-flex align-items-center text-white font-14">
-                            <i data-feather="mail" width="20" height="20" class="mr-10"></i>
-                            {{ $generalSettings['site_email'] }}
-                        </div>
-                    @endif
-                </div>
-            </div>
-        </div>
-    @endif
 
 </footer>
